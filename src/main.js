616936d5cbaea3e94330b92593ab5d0507237b2e
Denae <dfickes@ucsc.edu>
2022-04-07 15:52:01 -0700

    Rocket Patrol

--- src/main.js
+++ src/main.js
@@ -1,9 +1,1 @@
-console.log("Hello from main.js");
-
-let config = {
-    type: Phaser.CANVAS,
-    width: 640, 
-    height: 480
-}
-
-let game = new Phaser.Game(config);
+console.log("hello world");
